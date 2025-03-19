3c765fca93b9c5bba441c896bc33199c7c3b5428
shRamim13 <shramim13@gmail.com>
2025-03-19 15:41:18 +0600

    first commit

---
+++ src/config/db.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const logger = require('../utils/helpers').logger;
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+// Handle MongoDB events
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB error:', err);
+});
+
+process.on('SIGINT', async () => {
+    await mongoose.connection.close();
+    process.exit(0);
+});
+
+module.exports = connectDB;
